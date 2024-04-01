a46df2a9382006f95252f2eac3e43f254d5547bc
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-01 16:07:44 -0400

    Questão 11 concluida

---
+++ Questao-11.js
@@ -0,0 +1,13 @@
+/*
+            Estrutura de repetição do...while: 
+11. Questão: Escreva um código que imprima os números de 1 a 5.  
+*/
+
+let numero = 1
+
+do {
+    console.log("numero: " + numero)
+    numero++;
+}
+
+while (numero <= 5)
