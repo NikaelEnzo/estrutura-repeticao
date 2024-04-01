9b79b0c1e70f39264ece42538a9cf49ea12e0376
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-01 16:11:43 -0400

    Questão 13 concluida

---
+++ Questao-13.js
@@ -0,0 +1,14 @@
+/*
+            Estrutura de repetição do...while: 
+13. Questão: Escreva um código que imprima os números de 10 a 1 em 
+ordem decrescente. 
+*/
+
+let numero = 10
+
+do {
+    console.log("numero: " + numero)
+    numero--;
+}
+
+while (numero >= 1)
