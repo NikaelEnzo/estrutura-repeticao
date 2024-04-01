40962ee78f81c9bda45f9475b73fa89177e05113
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-01 15:33:45 -0400

    Questão 01 e 05 concluida

---
+++ Questao-01.js
@@ -0,0 +1,8 @@
+/*
+            Estrutura de repetição for:
+1. Questão: Escreva um código que imprima os números de 1 a 5.
+*/
+
+for(let numero = 1; numero <= 5; numero++){
+    console.log("Valor de número = " + numero)
+}
---
+++ Questao-05.js
@@ -0,0 +1,8 @@
+/*
+            Estrutura de repetição for:
+5. Questão: Escreva um código que imprima a tabuada do 5 (de 1 a 10).  
+*/
+
+for(let numero = 1; numero <= 10; numero++){
+    console.log("Valor de número = " + numero * 5)
+}
