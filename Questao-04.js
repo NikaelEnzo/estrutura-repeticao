9e2cf029a173a56ae17ac556a43e9e643f368935
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-01 15:01:56 -0400

    Questão 04 concluida

---
+++ Questao-04.js
@@ -0,0 +1,9 @@
+/*
+            Estrutura de repetição for:
+4. Questão: Escreva um código que imprima os números de 0 a 100 em 
+incrementos de 10.
+*/
+
+for(let numero = 0; numero <= 100; numero += 10){
+    console.log("Valor de número = " + numero)
+}
