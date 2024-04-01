56d8595d3700868373b42d2c7e6ab8b699b7a34d
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-01 14:54:12 -0400

    Questão 02 concluida

---
+++ Questao-02.js
@@ -0,0 +1,10 @@
+/*
+            Estrutura de repetição for:
+2. Questão: Escreva um código que imprima os números pares de 2 a 10.
+*/
+
+for(let numero = 2; numero <= 10; numero++){
+    if (numero % 2 == 0) {
+        console.log("Valor de número = " + numero)
+    }
+}
