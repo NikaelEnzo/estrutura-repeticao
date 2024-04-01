e9d8b81f860e89099c61aefda5650d8905937843
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-01 15:47:54 -0400

    Questão 09 concluida

---
+++ Questao-09.js
@@ -0,0 +1,12 @@
+/*
+            Estrutura de repetição while: 
+9. Questão: Escreva um código que imprima os números de 0 a 100 em 
+incrementos de 10.
+*/
+
+let numero = 0
+
+while (numero <= 100) {
+        console.log("numero: " + numero);
+    numero += 10;
+}
