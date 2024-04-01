ec765de03ac0121451c011a9d0f0c76e73697a19
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-01 16:29:34 -0400

    Questão 14 concluida

---
+++ Questao-14.js
@@ -0,0 +1,14 @@
+/*
+            Estrutura de repetição do...while: 
+14. Questão: Escreva um código que imprima os números de 0 a 100 em 
+incrementos de 10.  
+*/
+
+let numero = 0
+
+do {
+    console.log("numero: " + numero)
+    numero += 10;
+}
+
+while (numero <= 100)
