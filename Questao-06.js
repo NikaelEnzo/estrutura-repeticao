2a78f99610d21d751bad769962c269052a415131
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-01 15:43:15 -0400

    Questão 06 concluida

---
+++ Questao-06.js
@@ -0,0 +1,11 @@
+/*
+            Estrutura de repetição while: 
+6. Questão: Escreva um código que imprima os números de 1 a 5.
+*/
+
+let numero = 1
+
+while (numero <= 5) {
+    console.log("numero: " + numero);
+    numero++;
+}
