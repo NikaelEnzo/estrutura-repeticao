b91ec50fadcc30aa6f0d13719ba497620eab3c81
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-01 14:59:22 -0400

    Questão 03 concluida

---
+++ Questao-03.js
@@ -0,0 +1,9 @@
+/*
+            Estrutura de repetição for:
+3. Questão: Escreva um código que imprima os números de 10 a 1 em 
+ordem decrescente. 
+*/
+
+for(let numero = 10; numero >= 1; numero--){
+    console.log("Valor de número = " + numero)
+}
