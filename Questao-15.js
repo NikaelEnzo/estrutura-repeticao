ba7a89c1982e3031530e4a51c8230ed97ec4ffea
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-01 16:30:26 -0400

    Questão 15 concluida

---
+++ Questao-15.js
@@ -0,0 +1,13 @@
+/*
+            Estrutura de repetição do...while: 
+15. Questão: Escreva um código que imprima a tabuada do 5 (de 1 a 10).  
+*/
+
+let numero = 1
+
+do {
+    console.log("numero: " + numero * 5)
+    numero++;
+}
+
+while (numero <= 10)
