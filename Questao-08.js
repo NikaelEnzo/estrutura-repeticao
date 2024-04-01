efbe2dbe60fc48739fb8d96f6a05c2ddab01ab46
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-01 15:46:54 -0400

    Questão 08 concluida

---
+++ Questao-08.js
@@ -0,0 +1,12 @@
+/*
+            Estrutura de repetição while: 
+8. Questão: Escreva um código que imprima os números de 10 a 1 em 
+ordem decrescente. 
+*/
+
+let numero = 10
+
+while (numero >= 1) {
+        console.log("numero: " + numero);
+    numero--;
+}
